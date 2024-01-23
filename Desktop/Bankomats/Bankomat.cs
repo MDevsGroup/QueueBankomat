8ff5c4f1a4b4183d28681d54e615396437c7fd9d
Mirabbos Egamberdiyev <mirabbosegamberdiyev7@gmail.com>
2024-01-24 01:13:09 +0500

    Hech narsa o'zgarmadi

--- Desktop/Bankomats/Bankomat.cs
+++ Desktop/Bankomats/Bankomat.cs
@@ -26,17 +26,20 @@ namespace Desktop.Bankomats
 
         private void CheckSelected_Sh(object? sender, EventArgs e)
         {
-            _tanlangan = shifokorlar.Where(sh => sh.IsSelected == true)
-                                       .OrderByDescending(sh => sh.SelectedTime)
-                                       .FirstOrDefault();
-            if (_tanlangan != null)
+            Task task = Task.Run(() =>
             {
-                tanlanganSh.Text = $"Tanlandi: {_tanlangan.FullName}";
-            }
-            else
-            {
-                tanlanganSh.Text = "";
-            }
+                _tanlangan = shifokorlar.Where(sh => sh.IsSelected == true)
+                            .OrderByDescending(sh => sh.SelectedTime)
+                            .FirstOrDefault();
+                if (_tanlangan != null)
+                {
+                    tanlanganSh.Text = $"Tanlandi: {_tanlangan.FullName}";
+                }
+                else
+                {
+                    tanlanganSh.Text = "";
+                }
+            });
         }
 
         private async void guna2Button1_Click(object sender, EventArgs e)
@@ -74,14 +77,19 @@ namespace Desktop.Bankomats
 
         private void Bankomat_Load(object sender, EventArgs e)
         {
-            Timer timer1 = new();
-            timer1.Interval = 1000;
-            timer1.Tick += guna2Button1_Click;
-            timer1.Start();
+            Task task = Task.Run(() =>
+            {
+                Timer timer1 = new();
+                timer1.Interval = 1000;
+                timer1.Tick += guna2Button1_Click;
+                timer1.Start();
+            });
+
         }
 
         private async void NavbatBtn_Click(object sender, EventArgs e)
         {
+         
             _tanlangan.IsSelected = false;
             await _clientInterface.Create(_tanlangan.UserId, DateTime.Now);
         }
--- Desktop/Bankomats/Shifokorlar.cs
+++ Desktop/Bankomats/Shifokorlar.cs
@@ -25,7 +25,11 @@ public partial class Shifokorlar : UserControl
 
     private async void Shifokorlar_Load(object sender, EventArgs e)
     {
-        FormLoad();
+        Task task = Task.Run(() =>
+        {
+            FormLoad();
+
+        });
     }
 
     private void guna2Panel1_Click(object sender, EventArgs e)
